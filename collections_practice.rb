53a3fe17bbeb6926fd252092a290186ae6295ab9
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-12 23:57:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|word| word.start_with? ("r")}
+end
+
+def contain_a(array)
+  array.find_all {|word| word.include? ("a")}
+end
+
+def first_wa(array)
+  array.find {|prefix| prefix.include? ("wa")}
+end
+
+def remove_non_strings(array)
+end
+
+def count_elements(array)
+end
+
+def merge_data(array1, array2)
+end
+
+def find_cool
+end
+
+def organize_schools
+end
